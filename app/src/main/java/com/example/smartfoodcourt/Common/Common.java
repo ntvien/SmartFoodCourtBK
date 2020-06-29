2cf04a1e9204485754ec37cafdfe697e17b44ddb
ntvien <62584322+ntvien@users.noreply.github.com>
2020-06-29 10:33:31 +0700

    add var for remember password

--- app/src/main/java/com/example/smartfoodcourt/Common/Common.java
+++ app/src/main/java/com/example/smartfoodcourt/Common/Common.java
@@ -6,4 +6,6 @@ public class Common {
     public static User currentUser;
 
     public static final String INTENT_FOOD_ID = "FoodID";
+    public static final String USER_KEY = "User";
+    public static final String PASSWORD_KEY = "Password";
 }
