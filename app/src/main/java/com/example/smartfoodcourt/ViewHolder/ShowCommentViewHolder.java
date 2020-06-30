816b8a5ed76832f2511504d09b815225816854f4
ntvien <62584322+ntvien@users.noreply.github.com>
2020-06-30 23:20:59 +0700

    Update ShowCommentViewHolder.java
    add button delete comment

--- app/src/main/java/com/example/smartfoodcourt/ViewHolder/ShowCommentViewHolder.java
+++ app/src/main/java/com/example/smartfoodcourt/ViewHolder/ShowCommentViewHolder.java
@@ -1,6 +1,7 @@
 package com.example.smartfoodcourt.ViewHolder;
 
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
@@ -13,10 +14,13 @@ public class ShowCommentViewHolder extends RecyclerView.ViewHolder {
     public TextView txtUserPhone, txtComment;
     public RatingBar ratingBarDetail;
 
+    public ImageView btn_delete_comment;
+
     public ShowCommentViewHolder(@NonNull View itemView) {
         super(itemView);
         txtUserPhone = (TextView)itemView.findViewById(R.id.txtUserPhone);
         txtComment = (TextView)itemView.findViewById(R.id.txtComment);
         ratingBarDetail = (RatingBar)itemView.findViewById(R.id.ratingBarDetail);
+        btn_delete_comment = (ImageView)itemView.findViewById(R.id.btn_delete_comment);
     }
 }
