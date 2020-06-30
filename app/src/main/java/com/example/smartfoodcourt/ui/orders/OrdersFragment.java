9858276dc8d79430792bec30f16565842acc7f0b
ntvien <62584322+ntvien@users.noreply.github.com>
2020-06-30 23:19:48 +0700

    fixed

--- app/src/main/java/com/example/smartfoodcourt/ui/orders/OrdersFragment.java
+++ app/src/main/java/com/example/smartfoodcourt/ui/orders/OrdersFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -13,10 +14,15 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.smartfoodcourt.Common.Common;
+import com.example.smartfoodcourt.Model.Food;
 import com.example.smartfoodcourt.Model.Order;
 import com.example.smartfoodcourt.R;
+import com.example.smartfoodcourt.ViewHolder.FoodViewHolder;
 import com.example.smartfoodcourt.ViewHolder.OrderViewHolder;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -62,42 +68,110 @@ public class OrdersFragment extends Fragment {
     }
 
     private void loadOrders(String phone) {
-        adapter = new FirebaseRecyclerAdapter<Order, OrderViewHolder>(
-                Order.class,
-                R.layout.order_layout,
-                OrderViewHolder.class,
-                orders.orderByChild("phone").equalTo(phone)
-        ) {
+
+        FirebaseRecyclerOptions<Order> options = new FirebaseRecyclerOptions.Builder<Order>()
+                .setQuery(orders.orderByChild("phone").equalTo(phone), Order.class).build();
+        adapter = new FirebaseRecyclerAdapter<Order, OrderViewHolder>(options) {
             @Override
-            protected void populateViewHolder(OrderViewHolder orderViewHolder, Order model, final int position) {
+            protected void onBindViewHolder(@NonNull OrderViewHolder orderViewHolder, final int position, @NonNull Order order) {
                 orderViewHolder.txtOrderId.setText(adapter.getRef(position).getKey());
-                orderViewHolder.txtOrderStatus.setText(convertCodeToStatus(model.getStatus()));
-                orderViewHolder.txtOrderPhone.setText(model.getPhone());
-
-                orderViewHolder.btnEdit.setOnClickListener(new View.OnClickListener() {
+                orderViewHolder.txtOrderStatus.setText(convertCodeToStatus(order.getStatus()));
+                orderViewHolder.txtOrderPhone.setText(order.getPhone());
+                orderViewHolder.btn_delete.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        //showUpdateDialog(adapter.getRef(position).getKey(), adapter.getItem(position));
-                    }
-                });
-
-                orderViewHolder.btnDetail.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
+                        if(adapter.getItem(position).getStatus().equals("0"))
+                            deleteOrders(adapter.getRef(position).getKey());
+                        else
+                            Toast.makeText(getContext(), "You cannot delete this Order!!!",Toast.LENGTH_SHORT).show();
 
                     }
                 });
 
-                orderViewHolder.btnRemove.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        deleteOrder(adapter.getRef(position).getKey());
-                    }
-                });
+//                orderViewHolder.btnEdit.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View view) {
+//                        //showUpdateDialog(adapter.getRef(position).getKey(), adapter.getItem(position));
+//                    }
+//                });
+//
+//                orderViewHolder.btnDetail.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View view) {
+//
+//                    }
+//                });
+//
+//                orderViewHolder.btnRemove.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View view) {
+//                        deleteOrder(adapter.getRef(position).getKey());
+//                    }
+//                });
+            }
+
+            @NonNull
+            @Override
+            public OrderViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.order_layout, parent, false);
+                return new OrderViewHolder(itemView);
             }
         };
+
+
+//        adapter = new FirebaseRecyclerAdapter<Order, OrderViewHolder>(
+//                Order.class,
+//                R.layout.order_layout,
+//                OrderViewHolder.class,
+//                orders.orderByChild("phone").equalTo(phone)
+//        ) {
+//            @Override
+//            protected void populateViewHolder(OrderViewHolder orderViewHolder, Order model, final int position) {
+//                orderViewHolder.txtOrderId.setText(adapter.getRef(position).getKey());
+//                orderViewHolder.txtOrderStatus.setText(convertCodeToStatus(model.getStatus()));
+//                orderViewHolder.txtOrderPhone.setText(model.getPhone());
+//
+//                orderViewHolder.btnEdit.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View view) {
+//                        //showUpdateDialog(adapter.getRef(position).getKey(), adapter.getItem(position));
+//                    }
+//                });
+//
+//                orderViewHolder.btnDetail.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View view) {
+//
+//                    }
+//                });
+//
+//                orderViewHolder.btnRemove.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View view) {
+//                        deleteOrder(adapter.getRef(position).getKey());
+//                    }
+//                });
+//            }
+//        };
+
         adapter.notifyDataSetChanged();
+        adapter.startListening();
         recyclerView.setAdapter(adapter);
+    }
+
+    private void deleteOrders(final String key) {
+        orders.child(key).removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Toast.makeText(getContext(),new StringBuilder("Order").append(key).append("has been deleted").toString(),Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getContext(),e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
     private void deleteOrder(String key) {
@@ -105,7 +179,13 @@ public class OrdersFragment extends Fragment {
         adapter.notifyDataSetChanged();
     }
 
-//    private void showUpdateDialog(String key, Order item) {
+    @Override
+    public void onStop() {
+        super.onStop();
+       // adapter.stopListening();
+    }
+
+    //    private void showUpdateDialog(String key, Order item) {
 //        final AlertDialog.Builder alertDialog = new AlertDialog.Builder(OrdersFragment.class);
 //        alertDialog.setTitle("Update Order");
 //        alertDialog.setMessage("Please choose status");
