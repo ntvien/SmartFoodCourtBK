c95af593c68f475f284f9d0618e1cbfbf9e28e88
ntvien <62584322+ntvien@users.noreply.github.com>
2020-08-09 09:16:04 +0700

    update

--- app/src/main/java/com/example/smartfoodcourt/Fragment/HomeFragment.java
+++ app/src/main/java/com/example/smartfoodcourt/Fragment/HomeFragment.java
@@ -101,7 +101,6 @@ public class HomeFragment extends Fragment {
                 return new StallViewHolder(itemView);
             }
         };
-
         adapterStall.notifyDataSetChanged();
         stallRecycler.setAdapter(adapterStall);
     }
